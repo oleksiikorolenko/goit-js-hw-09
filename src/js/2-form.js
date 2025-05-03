038aeb4c27cac83b196d67117a53d301c7d511c2
Korolenko <korolenkoalexey84@gmail.com>
2025-05-03 21:16:28 +0300

    first commit

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+const STORAGE_KEY = "feedback-form-state";
+let formData = { email: "", message: "",};
+const form = document.querySelector(".feedback-form");
+const textarea = form.elements.message;
+const emailInput = form.elements.email;
+
+
+
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedData) {
+    emailInput.value = savedData.email || " ";
+    textarea.value = savedData.message || " ";
+}
+
+form.addEventListener('input', handleFormInput);
+
+function handleFormInput(e) {
+    formData.email = emailInput.value.trim();
+    formData.message = textarea.value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+form.addEventListener('submit', handleFormSubmit)
+function handleFormSubmit(e) {
+    e.preventDefault();
+    const email = emailInput.value.trim();
+    const message = textarea.value.trim();
+    if (!email || !message) {
+        
+        alert("Fill please all fields");
+        return;
+    
+}
+
+    const form = e.currentTarget;
+    localStorage.removeItem(STORAGE_KEY);
+
+    form.reset()
+}
